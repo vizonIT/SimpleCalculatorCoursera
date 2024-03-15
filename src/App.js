26312e38ed4e094bce9913931763bfde001f7244
vizonIT <osukakvick@gmail.com>
2024-03-15 20:21:00 +0100

    Commit to calculator App

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import {
+  useState,
+  useRef
+} from "react";
+import "./App.css";
+
+function App() {
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  const [result, setResult] = useState(0);
+
+  function plus(e) {
+    e.preventDefault();
+    setResult((result) => result + Number(inputRef.current.value));
+  };
+
+  function minus(e) {
+    // Add the code for the minus function 
+    e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value));
+  };
+
+  function times(e) {
+    // Add the code for the plus function
+    e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value));
+  };
+
+  function divide(e) {
+    // Add the code for the divide function
+    e.preventDefault();
+    setResult((result) => result / Number(inputRef.current.value));
+  };
+
+  function resetInput(e) {
+    // Add the code for the resetInput function
+    e.preventDefault();
+    inputRef.current.value = '';
+  };
+
+  function resetResult(e) {
+    // Add the code for the resetResult function
+    e.preventDefault();
+    setResult((prevVal) => prevVal = 0);
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Simplest Working Calculator</h1>
+      </div>
+      <form>
+        <p ref={resultRef}>Result: {result}</p>
+        <input
+          pattern="[0-9]"
+          ref={inputRef}
+          type="number"
+          placeholder="Type a number"
+        />
+        <button onClick={plus}>add</button>
+        <button onClick={minus}>Subtract</button>
+        <button onClick={times}>Multiply</button>
+        <button onClick={divide}>Divide</button>
+        <button onClick={resetInput}>Reset Input</button>
+        <button onClick={resetResult}>Reset Result</button>
+      </form>
+    </div>
+  );
+}
+
+export default App; 
